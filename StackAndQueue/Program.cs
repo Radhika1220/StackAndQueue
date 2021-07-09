9a12d83c06ed15a1b664fe631b7d070cd2e6db88
Radhika <radhika.shankar1220@gmail.com>
2021-07-09 11:42:07 +0530

    [Radhika] Enqueue operation

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -12,10 +12,11 @@ namespace StackAndQueue
         {
             Console.WriteLine("-------------Stack Operations-------------");
             string flag = "Y";
-            while (flag == "Y" || flag=="y")
+            while (flag == "Y" || flag == "y")
             {
                 Console.WriteLine("***1.Push Operation***");
                 Console.WriteLine("***2.Peek And Pop Operation");
+                Console.WriteLine("3.------------Queue Operations-----------");
                 Console.WriteLine("Enter the option");
                 int num = Convert.ToInt32(Console.ReadLine());
                 Stack stack = new Stack();
@@ -34,13 +35,39 @@ namespace StackAndQueue
                         stack.Pop();
                         stack.Display();
                         break;
+                    case 3:
+                        string flag1 = "Y";
+                        while (flag1 == "Y" || flag1 == "y")
+                        {
+                            Console.WriteLine("***1.Enqueue Operation***");
+                            Console.WriteLine("Enter the option");
+                            int num1 = Convert.ToInt32(Console.ReadLine());
+                            Queue queue = new Queue();
+                            switch (num1)
+                            {
+                                case 1:
+                                    queue.Enqueue(56);
+                                    queue.Enqueue(30);
+                                    queue.Enqueue(70);
+                                    queue.Display();
+                                    break;
+                                default:
+                                    Console.WriteLine("Enter a valid option");
+                                    break;
+                            }
+                            Console.WriteLine("\nDo you want to continue?(Y/N)");
+                            flag1 = Console.ReadLine();
+                        }
+                        break;
                     default:
-                        Console.WriteLine("Enter a valid option");
+                        Console.WriteLine("Enter a Valid Option!!!");
                         break;
                 }
                 Console.WriteLine("\nDo you want to continue?(Y/N)");
-                flag=Console.ReadLine();
+                flag = Console.ReadLine();
             }
+            Console.ReadKey();
         }
     }
 }
+  
