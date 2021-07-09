7d6b6f2ace5ce2c343a36924666389554b21f786
Radhika <radhika.shankar1220@gmail.com>
2021-07-09 11:05:22 +0530

    [Radhika]Peek and pop operation

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -11,23 +11,36 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("-------------Stack Operations-------------");
-            Console.WriteLine("***1.Push operation***");
-            Console.WriteLine("Enter the option");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Stack stack = new Stack();
-            switch(num)
+            string flag = "Y";
+            while (flag == "Y" || flag=="y")
             {
-                case 1:
-                    stack.Push(70);
-                    stack.Push(30);
-                    stack.Push(56);
-                    stack.Display();
-                    break;
-                default:
-                    Console.WriteLine("Enter a valid option");
-                    break;
+                Console.WriteLine("***1.Push Operation***");
+                Console.WriteLine("***2.Peek And Pop Operation");
+                Console.WriteLine("Enter the option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Stack stack = new Stack();
+                switch (num)
+                {
+                    case 1:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.Display();
+                        break;
+                    case 2:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.Pop();
+                        stack.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Enter a valid option");
+                        break;
+                }
+                Console.WriteLine("\nDo you want to continue?(Y/N)");
+                flag=Console.ReadLine();
             }
-            Console.ReadLine();
         }
     }
 }
